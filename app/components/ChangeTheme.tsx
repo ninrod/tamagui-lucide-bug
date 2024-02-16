9ec2f13a89bd7321171159faf1ea26fda8374c47
ninrod <filipe.silva@gmail.com>
2024-02-16 17:46:19 -0300

    working, before the uncomment

--- app/components/ChangeTheme.tsx
+++ app/components/ChangeTheme.tsx
@@ -1,8 +1,11 @@
 import { XStack } from "tamagui";
+import { Sun } from "@tamagui/lucide-icons"
 
 export function ChangeTheme() {
     return (
         <XStack>
+             {/* <Sun/> */}
+             {/* uncomment the above line and watch everything blow up */}
         </XStack>
     )
 }
